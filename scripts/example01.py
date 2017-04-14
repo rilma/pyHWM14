17fdd97f64c75f81a720458638305bf48452086a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-14 15:12:27 -0400

    pythonic

--- scripts/example01.py
+++ scripts/example01.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
-from pyhwm2014.pyhwm14 import HWM14, HWM14Plot
+from pyhwm2014 import HWM14, HWM14Plot
 
 def example01():
-    # Single Height profile    
+    # Single Height profile
     hwm14Obj = HWM14( altlim=[90,200], altstp=1, ap=[-1, 35], day=323,
         option=1, ut=11.66667, verbose=False, year=1993 )
-        
+
     # Height profile plot
     hwm14Gbj = HWM14Plot( profObj=hwm14Obj )
 
 
-if __name__ == '__main__':
-
-    example01()
+example01()
--- setup.py
+++ setup.py
@@ -4,30 +4,29 @@ from glob import glob
 from os.path import join
 from numpy.distutils.core import Extension, setup
 
+req = ['numpy','matplotlib']
+
 name = 'pyhwm2014'
 
 ext = Extension( extra_compile_args=['-w'],
             extra_f90_compile_args=['-w'],
-            f2py_options=[ '--quiet' ], 
-            name='hwm14', 
+            f2py_options=[ '--quiet' ],
+            name='hwm14',
             sources=['source/hwm14.pyf', 'source/hwm14.f90']
              )
 
 hwmData1 = glob(join('data', '*.dat'))
 hwmData2 = glob(join('data', '*.bin'))
-hwmDataFiles = [(join(name, 'data'), hwmData1), 
+hwmDataFiles = [(join(name, 'data'), hwmData1),
                 (join(name, 'data'), hwmData2)]
 
-if __name__ == '__main__':
-
-    setup( author='Ronald Ilma', 
-        author_email='rri5@cornell.edu',
-        data_files=hwmDataFiles, 
-        description='HWM14 neutral winds model', 
+setup( author=['Ronald Ilma','Michael Hirsch, Ph.D'],
+        data_files=hwmDataFiles,
+        description='HWM14 neutral winds model',
         ext_modules=[ ext ],
-        ext_package=name, 
-        name=name, 
-        packages=[name],  
-        url='https://github.com/rilma/pyHWM14', 
-        version='0.0.2',
+        ext_package=name,
+        name=name,
+        packages=[name],
+        url='https://github.com/rilma/pyHWM14',
+        version='1.0.01',
         )
